69b308e6b273173525fe7ac3b5e1fcaf8f9fe8b5
Rollin Francois <Rollin@rollins-macbook-pro.local>
2017-09-02 16:03:42 -0400

    Add coder to receive location updates to the View Controller class.

--- CLTest/ViewController.swift
+++ CLTest/ViewController.swift
@@ -7,8 +7,16 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // MARK: Properties
+    
+    var locationManager: CLLocationManager? = nil
+    var lastLocation: CLLocation? = nil
+    var isReceivingLocationUpdates: Bool = false
+    
     
     // MARK: Outlets
     
@@ -21,13 +29,83 @@ class ViewController: UIViewController {
     // MARK: Action Methods
     
     @IBAction func onButtonPressed(_ sender: Any) {
+        
+        if isReceivingLocationUpdates == false {
+            
+            if  CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse {
+                locationManager!.requestWhenInUseAuthorization()
+            } else  {
+                isReceivingLocationUpdates = true
+                toggleButton.titleLabel!.text = "Stop Location Updates"
+                locationManager!.startUpdatingLocation()
+            }
+        } else {
+            isReceivingLocationUpdates = false
+            toggleButton.titleLabel!.text = "Start Location Updates"
+            locationManager?.stopUpdatingLocation()
+        }
     }
     
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        var shouldAllow = false
+        
+        switch status {
+        case CLAuthorizationStatus.authorizedWhenInUse:
+            shouldAllow = true
+        case CLAuthorizationStatus.authorizedAlways:
+            shouldAllow = true
+        default:
+            shouldAllow = false
+        }
+        
+        if shouldAllow == true {
+            isReceivingLocationUpdates = true
+            toggleButton.titleLabel!.text = "Stop Location Updates"
+            manager.startUpdatingLocation()
+        }
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let locationArray = locations as NSArray
+        
+        for newLocation in locationArray {
+            
+            // lat/lon  values should only be considered if 
+            // horizontal accuracy is not negative.
+            if (newLocation as AnyObject).horizontalAccuracy >= 0 {
+                let currentLatitude: CLLocationDegrees = (newLocation as AnyObject).coordinate.latitude
+                let currentLongitude: CLLocationDegrees = (newLocation as AnyObject).coordinate.longitude
+                
+                let distanceTravelled = (newLocation as AnyObject).distance(from: lastLocation!)
+                
+                
+                
+                latitudeValue.text = "\(currentLatitude)"
+                LongitudeValue.text = "\(currentLongitude)"
+                distanceValue.text = "\(distanceTravelled)"
+                
+                lastLocation = newLocation as? CLLocation
+                
+            }
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager!.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        lastLocation = CLLocation(latitude: 51.5001524, longitude: -0.1262362)
+        
+        toggleButton.titleLabel!.text = "Stop Location Updates"
     }
 
     override func didReceiveMemoryWarning() {
